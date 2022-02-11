1c375d179473d2461b75f2b70cfd42cec761e57f
Code Sandbox <hello@codesandbox.io>
2022-02-11 15:18:55 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.1a440d71894a49415779d10947243751.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c456543bda83726283bafa98ec2a9e8",
+    "url": "https://wardbeyens.github.io/csb-0fh11/index.html"
+  },
+  {
+    "revision": "7195e50fd7ef6d9c5207",
+    "url": "https://wardbeyens.github.io/csb-0fh11/static/css/2.c1c45ef1.chunk.css"
+  },
+  {
+    "revision": "e9e406dcb9ada68a1c2a",
+    "url": "https://wardbeyens.github.io/csb-0fh11/static/css/main.23823db4.chunk.css"
+  },
+  {
+    "revision": "7195e50fd7ef6d9c5207",
+    "url": "https://wardbeyens.github.io/csb-0fh11/static/js/2.731c1c26.chunk.js"
+  },
+  {
+    "revision": "e9e406dcb9ada68a1c2a",
+    "url": "https://wardbeyens.github.io/csb-0fh11/static/js/main.4e48960a.chunk.js"
+  },
+  {
+    "revision": "a255bce3895c98253c46",
+    "url": "https://wardbeyens.github.io/csb-0fh11/static/js/runtime~main.c35edced.js"
+  }
+]);
